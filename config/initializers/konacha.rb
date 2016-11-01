1cd6ecbaf32a20231a70c7e37fd9413036811732
Hugo Corbucci <hugo.corbucci@gmail.com>
2016-10-31 17:02:52 -0700

    Upgrading dev gems
    Fixing a new rubocop rule

--- config/initializers/konacha.rb
+++ config/initializers/konacha.rb
@@ -1,5 +1,7 @@
 # encoding: UTF-8
-Konacha.configure do |config|
-  require 'capybara/poltergeist'
-  config.driver = :poltergeist
-end if defined?(Konacha)
+if defined?(Konacha)
+  Konacha.configure do |config|
+    require 'capybara/poltergeist'
+    config.driver = :poltergeist
+  end
+end
